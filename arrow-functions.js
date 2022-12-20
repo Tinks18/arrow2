0e18b611fba8db472dc92fa7032f463c2466465f
Tinks18 <tinutani9@gmail.com>
2022-12-20 02:07:06 +0000

    Final commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,46 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a,b) {
+
+//     return a+b;
+    
+// }
+
+// let sum = addTwoNumbers(3,5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a,b) => {
+
+    return a+b;
+    
+}
+
+let sum = addTwoNumbers(3,5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a,b) => a+b;
+let sum2 = addTwoNumbers2(4,5);
+console.log(sum2);
+
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hello world!");
+
+const sayHello = () => console.log("Hi everyone!");
+sayHello();
+
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    "Every story has a 
+    the end 
+    and beginning 
+    and a middle"
+    </p>`)
+    console.log(returnMultipleLines());
+sayHello();
